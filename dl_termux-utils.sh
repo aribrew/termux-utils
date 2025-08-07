b26ed3d960caa1e57bdd51f102a98cee73099ce2
aribrew <arithesage@protonmail.com>
2025-08-07 12:20:21 +0200

    Fixing permissions. Added script for aiding in the initial repository cloning.

---
+++ dl_termux-utils.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+TERMUX_UTILS_REPO="https://github.com/aribrew/termux-utils.git"
+
+
+if ! [[ -d "$HOME/termux-utils" ]];
+then
+	pkg update
+
+
+	if [[ "$?" == "0" ]];
+	then
+	    pkg install -y git
+
+	    if [[ "$?" == "0" ]];
+	    then
+	        git clone $TERMUX_UTILS_REPO $HOME/termux-utils --depth 1
+
+	        if [[ "$?" == "0" ]];
+	        then
+	            echo "Termux Utils downloaded in ~/termux-utils."
+	            echo "Run ~/termux-utils/setup now."
+	            echo ""
+            fi
+        fi
+    fi
+fi
