4900e52195a293d10e31dcf5326037a2a9258eb0
aribrew <arithesage@protonmail.com>
2025-08-27 10:55:34 +0200

    Moving all the needed environment for Fluidsynth to the .midi_env file.

--- x11/midi_support/enable_midi.sh
+++ x11/midi_support/enable_midi.sh
@@ -6,30 +6,9 @@ SCRIPT_HOME=$(realpath $(dirname $0))
 source "$HOME/.midi_env"
 
 
-if ! [[ "$TERMUX_VERSION" == "" ]];
-then
-    SOUNDFONT="$SCRIPT_HOME/GeneralUser-GS.sf2"
-else
-    echo "TODO: Find system's soundfont."
-fi
-
-
-FLUIDSYNTH_OPTS="-a alsa -m alsa_seq -l "
-FLUIDSYNTH_OPTS+="-s \"$SOUNDFONT\""
-
-
 if [[ "$(which fluidsynth)" == "" ]];
 then
     echo "Install fluidsynth first."
-    echo ""
-
-    exit 1
-fi
-
-
-if ! [[ -f "$SOUNDFONT" ]];
-then
-    echo "Cannot find the desired soundfont '$SOUNDFONT'."
     echo ""
 
     exit 1
