23ad662140d934908d6a40def327363f2aa40e08
aribrew <arithesage@protonmail.com>
2025-09-04 10:35:34 +0200

    Mediatek CPUs seems to report CPU ID by Hardware rather than vendor_id.

--- x11/cpu_vendor.sh
+++ x11/cpu_vendor.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
-cat /proc/cpuinfo | grep -m 1  vendor_id | xargs | cut -d ':' -f 2 | xargs
+CPU_INFO=$(cat /proc/cpuinfo)
 
+VENDOR_ID=$(echo "$CPU_INFO" | grep -m 1 "vendor_id")
+
+if [[ "$VENDOR_ID" == "" ]];
+then
+    HARDWARE=$(echo "$CPU_INFO" | grep -m 1 "HARDWARE")
+
+    if [[ "$HARDWARE" == "" ]];
+    then
+        echo "UNKNOWN"
+    else
+        VENDOR=$(echo "$HARDWARE" | grep -m 1 "HARDWARE" | xargs)
+        VENDOR=$(echo "$VENDOR" | cut -d ':' -f 2 | xargs)
+        
+        if [[ $VENDOR == MT* ]];
+        then
+            echo "Mediatek"
+        else
+            echo "$VENDOR"
+        fi
+    fi
+else
+    VENDOR=$(echo "$VENDOR_ID" | grep -m 1 vendor_id | xargs)
+    VENDOR=$(echo "$VENDOR" | cut -d ':' -f 1 | xargs)
+
+    echo "$VENDOR"
+fi
