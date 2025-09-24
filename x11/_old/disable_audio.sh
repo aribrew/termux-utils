00502e33959e7a8f1dc3564191e15c95d5b8469e
aribrew <arithesage@protonmail.com>
2025-09-24 12:13:49 +0200

    This should work now. I was seeing another repo.

--- x11/_old/disable_audio.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Stopping running Pulseaudio server ..."
-kill -9 $(pgrep -f "pulseaudio") 2> /dev/null
-
--- x11/_old/start_xfce-native.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-screen -dmS desktop dbus-launch --exit-with-session xfce4-session
