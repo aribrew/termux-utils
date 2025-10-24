1ea0f018e2c9592daa86ab8176d31473c9b9659c
aribrew <arithesage@protonmail.com>
2025-10-24 22:12:43 +0200

    Setting up X11 requires more than one thing. It is better to use a script then.

---
+++ x11/for_termux/setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+source bash_helpers.sh
+
+if ! [[ -v BASH_HELPERS_LOADED ]];
+then
+    echo -e "BASH Helpers not found in PATH. Install it first.\n"
+    exit 1
+fi
+
+
+if [[ "$TERMUX_VERSION" == "" ]];
+then
+    abort "This script is intended for Termux only."
+fi
+
+
+if [[ -f "$PREFIX/bin/termux-x11" ]];
+then
+    abort "X11 already installed."
+fi
+
+
+echo "Refreshing package database..."
+echo "------------------------------"
+
+pkg update
+
+
+if ! [[ -f "$PREFIX/etc/apt/sources.list.d/tur.list" ]];
+then
+    echo "Installing Termux User Repository ..."
+    echo "-------------------------------------"
+    
+    pkg install tur-repo -y
+fi
+
+
+if ! [[ -f "$PREFIX/etc/apt/sources.list.d/x11.list" ]];
+then
+    echo "Installing X11 repository ..."
+    echo "-----------------------------"
+
+    pkg install x11-repo -y
+fi
+
+
+if ! [[ -f "$PREFIX/bin/termux-x11" ]];
+then
+    echo "Installing Termux X11 server..."
+    echo "-------------------------------"
+
+    pkg install termux-x11-nightly -y
+fi
+
