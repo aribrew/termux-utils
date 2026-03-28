197788c69c5c5e4b10926d45599a1f9085c9f4c1
aribrew <arithesage@protonmail.com>
2026-03-28 13:18:02 +0100

    Trying to enable 3D acceleration.

---
+++ x11/for_termux/enable_3d_support.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if ! [[ -v 3D_ENV_LOADED ]];
+then
+    source "$HOME/termux-utils/x11/for_termux/.3d_env"
+fi
+
+
+VIRGL_OPTS="--use-egl-surfaceless"
+
+if ! [[ -v ZINK_COMPATIBILITY ]];
+then
+    VIRGL_OPTS+=" --use-gles"
+fi
+
+
+virgl_test_server $VIRGL_OPTS &
+
+echo -e "3D acceleration should be enabled now.\n"
+
